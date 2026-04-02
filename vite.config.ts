ff49e5a2d87ad496b08ad1d022c4587d2a52242f
phammyphuongbong <phammyphuongbong@gmail.com>
2026-04-03 00:30:34 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/MYRA-ArchiGen-AI/', // Tên repository của bạn trên GitHub
+})
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
